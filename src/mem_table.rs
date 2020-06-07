0e2522c343573053f9a41be6bf3d383f50be48e0
Adam Comer <adambcomer@gmail.com>
2020-06-06 21:33:00 -0400

    Updated MemTable to track  space used

--- src/mem_table.rs
+++ src/mem_table.rs
@@ -17,6 +17,7 @@ pub struct MemEntry {
 /// Entries are stored in a Vector over a HashMap to support Scans.
 pub struct MemTable {
   entries: Vec<MemEntry>,
+  size: usize,
 }
 
 impl MemTable {
@@ -24,6 +25,7 @@ impl MemTable {
   pub fn new() -> MemTable {
     return MemTable {
       entries: Vec::new(),
+      size: 0,
     };
   }
 
@@ -38,14 +40,25 @@ impl MemTable {
 
     match self.get_index(key) {
       Ok(idx) => {
+        // If a Value existed on the deleted record, then add the difference of the new and old Value to the MemTable's size.
+        if let Some(v) = self.entries[idx].value.as_ref() {
+          if value.len() < v.len() {
+            self.size -= v.len() - value.len();
+          } else {
+            self.size += value.len() - v.len();
+          }
+        }
         self.entries[idx] = entry;
       }
-      Err(idx) => self.entries.insert(idx, entry),
+      Err(idx) => {
+        self.size += key.len() + value.len() + 16 + 1; // Increase the size of the MemTable by the Key size, Value size, Timestamp size (16 bytes), Tombstone size (1 byte).
+        self.entries.insert(idx, entry)
+      }
     }
   }
 
   /// Deletes a Key-Value pair in the MemTable.
-  /// 
+  ///
   /// This is achieved using tombstones.
   pub fn delete(&mut self, key: &[u8], timestamp: u128) {
     let entry = MemEntry {
@@ -56,14 +69,21 @@ impl MemTable {
     };
     match self.get_index(key) {
       Ok(idx) => {
+        // If a Value existed on the deleted record, then subtract the size of the Value from the MemTable.
+        if let Some(value) = self.entries[idx].value.as_ref() {
+          self.size -= value.len();
+        }
         self.entries[idx] = entry;
       }
-      Err(idx) => self.entries.insert(idx, entry),
+      Err(idx) => {
+        self.size += key.len() + 16 + 1; // Increase the size of the MemTable by the Key size, Timestamp size (16 bytes), Tombstone size (1 byte).
+        self.entries.insert(idx, entry);
+      }
     }
   }
 
   /// Gets a Key-Value pair from the MemTable.alloc
-  /// 
+  ///
   /// If no record with the same key exists in the MemTable, return None.
   pub fn get(&self, key: &[u8]) -> Option<&MemEntry> {
     if let Ok(idx) = self.get_index(key) {
@@ -90,138 +110,131 @@ impl MemTable {
     return self.entries.len();
   }
 
-  /// Gets all of the Records from the MemTable.
+  /// Gets all of the records from the MemTable.
   pub fn entries(&self) -> &Vec<MemEntry> {
     return &self.entries;
+  }
+
+  /// Gets the total size of the records in the MemTable
+  pub fn size(&self) -> usize {
+    return self.size;
   }
 }
 
 #[cfg(test)]
 mod tests {
-  use crate::mem_table::{MemTable};
+  use crate::mem_table::MemTable;
   use std::time::{SystemTime, UNIX_EPOCH};
 
   #[test]
   fn test_mem_table_put_start() {
-    let timestamp = SystemTime::now()
-      .duration_since(UNIX_EPOCH)
-      .unwrap()
-      .as_micros();
     let mut table = MemTable::new();
-    table.set(b"Lime", b"Lime Smoothie", timestamp);
-    table.set(b"Orange", b"Orange Smoothie", timestamp + 10);
+    table.set(b"Lime", b"Lime Smoothie", 0); // 17 + 16 + 1
+    table.set(b"Orange", b"Orange Smoothie", 10); // 21 + 16 + 1
 
-    table.set(b"Apple", b"Apple Smoothie", timestamp + 20);
+    table.set(b"Apple", b"Apple Smoothie", 20); // 19 + 16 + 1
 
     assert_eq!(table.entries[0].key, b"Apple");
     assert_eq!(table.entries[0].value.as_ref().unwrap(), b"Apple Smoothie");
-    assert_eq!(table.entries[0].timestamp, timestamp + 20);
+    assert_eq!(table.entries[0].timestamp, 20);
     assert_eq!(table.entries[0].deleted, false);
     assert_eq!(table.entries[1].key, b"Lime");
     assert_eq!(table.entries[1].value.as_ref().unwrap(), b"Lime Smoothie");
-    assert_eq!(table.entries[1].timestamp, timestamp);
+    assert_eq!(table.entries[1].timestamp, 0);
     assert_eq!(table.entries[1].deleted, false);
     assert_eq!(table.entries[2].key, b"Orange");
     assert_eq!(table.entries[2].value.as_ref().unwrap(), b"Orange Smoothie");
-    assert_eq!(table.entries[2].timestamp, timestamp + 10);
+    assert_eq!(table.entries[2].timestamp, 10);
     assert_eq!(table.entries[2].deleted, false);
+
+    assert_eq!(table.size, 108);
   }
 
   #[test]
   fn test_mem_table_put_middle() {
-    let timestamp = SystemTime::now()
-      .duration_since(UNIX_EPOCH)
-      .unwrap()
-      .as_micros();
     let mut table = MemTable::new();
-    table.set(b"Apple", b"Apple Smoothie", timestamp);
-    table.set(b"Orange", b"Orange Smoothie", timestamp + 10);
+    table.set(b"Apple", b"Apple Smoothie", 0);
+    table.set(b"Orange", b"Orange Smoothie", 10);
 
-    table.set(b"Lime", b"Lime Smoothie", timestamp + 20);
+    table.set(b"Lime", b"Lime Smoothie", 20);
 
     assert_eq!(table.entries[0].key, b"Apple");
     assert_eq!(table.entries[0].value.as_ref().unwrap(), b"Apple Smoothie");
-    assert_eq!(table.entries[0].timestamp, timestamp);
+    assert_eq!(table.entries[0].timestamp, 0);
     assert_eq!(table.entries[0].deleted, false);
     assert_eq!(table.entries[1].key, b"Lime");
     assert_eq!(table.entries[1].value.as_ref().unwrap(), b"Lime Smoothie");
-    assert_eq!(table.entries[1].timestamp, timestamp + 20);
+    assert_eq!(table.entries[1].timestamp, 20);
     assert_eq!(table.entries[1].deleted, false);
     assert_eq!(table.entries[2].key, b"Orange");
     assert_eq!(table.entries[2].value.as_ref().unwrap(), b"Orange Smoothie");
-    assert_eq!(table.entries[2].timestamp, timestamp + 10);
+    assert_eq!(table.entries[2].timestamp, 10);
     assert_eq!(table.entries[2].deleted, false);
+
+    assert_eq!(table.size, 108);
   }
 
   #[test]
   fn test_mem_table_put_end() {
-    let timestamp = SystemTime::now()
-      .duration_since(UNIX_EPOCH)
-      .unwrap()
-      .as_micros();
     let mut table = MemTable::new();
-    table.set(b"Apple", b"Apple Smoothie", timestamp);
-    table.set(b"Lime", b"Lime Smoothie", timestamp + 10);
+    table.set(b"Apple", b"Apple Smoothie", 0);
+    table.set(b"Lime", b"Lime Smoothie", 10);
 
-    table.set(b"Orange", b"Orange Smoothie", timestamp + 20);
+    table.set(b"Orange", b"Orange Smoothie", 20);
 
     assert_eq!(table.entries[0].key, b"Apple");
     assert_eq!(table.entries[0].value.as_ref().unwrap(), b"Apple Smoothie");
-    assert_eq!(table.entries[0].timestamp, timestamp);
+    assert_eq!(table.entries[0].timestamp, 0);
     assert_eq!(table.entries[0].deleted, false);
     assert_eq!(table.entries[1].key, b"Lime");
     assert_eq!(table.entries[1].value.as_ref().unwrap(), b"Lime Smoothie");
-    assert_eq!(table.entries[1].timestamp, timestamp + 10);
+    assert_eq!(table.entries[1].timestamp, 10);
     assert_eq!(table.entries[1].deleted, false);
     assert_eq!(table.entries[2].key, b"Orange");
     assert_eq!(table.entries[2].value.as_ref().unwrap(), b"Orange Smoothie");
-    assert_eq!(table.entries[2].timestamp, timestamp + 20);
+    assert_eq!(table.entries[2].timestamp, 20);
     assert_eq!(table.entries[2].deleted, false);
+
+    assert_eq!(table.size, 108);
   }
 
   #[test]
   fn test_mem_table_put_overwrite() {
-    let timestamp = SystemTime::now()
-      .duration_since(UNIX_EPOCH)
-      .unwrap()
-      .as_micros();
     let mut table = MemTable::new();
-    table.set(b"Apple", b"Apple Smoothie", timestamp);
-    table.set(b"Lime", b"Lime Smoothie", timestamp + 10);
-    table.set(b"Orange", b"Orange Smoothie", timestamp + 20);
+    table.set(b"Apple", b"Apple Smoothie", 0);
+    table.set(b"Lime", b"Lime Smoothie", 10);
+    table.set(b"Orange", b"Orange Smoothie", 20);
 
-    table.set(b"Lime", b"A sour fruit", timestamp + 30);
+    table.set(b"Lime", b"A sour fruit", 30);
 
     assert_eq!(table.entries[0].key, b"Apple");
     assert_eq!(table.entries[0].value.as_ref().unwrap(), b"Apple Smoothie");
-    assert_eq!(table.entries[0].timestamp, timestamp);
+    assert_eq!(table.entries[0].timestamp, 0);
     assert_eq!(table.entries[0].deleted, false);
     assert_eq!(table.entries[1].key, b"Lime");
     assert_eq!(table.entries[1].value.as_ref().unwrap(), b"A sour fruit");
-    assert_eq!(table.entries[1].timestamp, timestamp + 30);
+    assert_eq!(table.entries[1].timestamp, 30);
     assert_eq!(table.entries[1].deleted, false);
     assert_eq!(table.entries[2].key, b"Orange");
     assert_eq!(table.entries[2].value.as_ref().unwrap(), b"Orange Smoothie");
-    assert_eq!(table.entries[2].timestamp, timestamp + 20);
+    assert_eq!(table.entries[2].timestamp, 20);
     assert_eq!(table.entries[2].deleted, false);
+
+    assert_eq!(table.size, 107);
   }
 
   #[test]
   fn test_mem_table_get_exists() {
-    let timestamp = SystemTime::now()
-      .duration_since(UNIX_EPOCH)
-      .unwrap()
-      .as_micros();
     let mut table = MemTable::new();
-    table.set(b"Apple", b"Apple Smoothie", timestamp);
-    table.set(b"Lime", b"Lime Smoothie", timestamp + 10);
-    table.set(b"Orange", b"Orange Smoothie", timestamp + 20);
+    table.set(b"Apple", b"Apple Smoothie", 0);
+    table.set(b"Lime", b"Lime Smoothie", 10);
+    table.set(b"Orange", b"Orange Smoothie", 20);
 
     let entry = table.get(b"Orange").unwrap();
 
     assert_eq!(entry.key, b"Orange");
     assert_eq!(entry.value.as_ref().unwrap(), b"Orange Smoothie");
-    assert_eq!(entry.timestamp, timestamp + 20);
+    assert_eq!(entry.timestamp, 20);
   }
 
   #[test]
@@ -249,6 +262,8 @@ mod tests {
     assert_eq!(table.entries[0].value, None);
     assert_eq!(table.entries[0].timestamp, 10);
     assert_eq!(table.entries[0].deleted, true);
+
+    assert_eq!(table.size, 22);
   }
 
   #[test]
@@ -264,5 +279,7 @@ mod tests {
     assert_eq!(table.entries[0].value, None);
     assert_eq!(table.entries[0].timestamp, 10);
     assert_eq!(table.entries[0].deleted, true);
+
+    assert_eq!(table.size, 22);
   }
 }
